0d43bb262a14af00db04f3a90acb9cd24bcff4d9
José Tomás Albornoz <jojo@eljojo.net>
2014-06-20 18:28:51 +0200

    Added github ssh public key to /home/app/.ssh/known_hosts

--- image/prepare.sh
+++ image/prepare.sh
@@ -10,3 +10,9 @@ usermod -L app
 mkdir -p /home/app/.ssh
 chmod 700 /home/app/.ssh
 chown app:app /home/app/.ssh
+
+# Getting to know GitHub
+ssh-keyscan github.com >> /home/app/.ssh/known_hosts
+chown app:app /home/app/.ssh/known_hosts
+chmod 0644 /home/app/.ssh/known_hosts
+
