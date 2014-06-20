927c1295856434723fda1575c1e17a65f638c69b
José Tomás Albornoz <jojo@eljojo.net>
2014-06-20 15:56:37 +0200

    Installing dev-headers package

--- image/install.sh
+++ image/install.sh
@@ -7,10 +7,9 @@ set -x
 /build/prepare.sh
 /build/utilities.sh
 
+/build/devheaders.sh
 /build/ruby2.1.sh
 /build/nodejs.sh
-
-# Must be installed after Ruby, so that we don't end up with two Ruby versions.
 /build/nginx.sh
 
 /build/finalize.sh
