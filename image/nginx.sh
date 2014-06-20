6ed27ad8174a1ca86ab2c38961a9ad48ef627239
José Tomás Albornoz <jojo@eljojo.net>
2014-06-20 15:53:21 +0200

    Removed passenger from nginx

--- image/nginx.sh
+++ image/nginx.sh
@@ -3,7 +3,7 @@ set -e
 source /build/buildconfig
 set -x
 
-apt-get install -y nginx-extras passenger
+apt-get install -y nginx-extras
 cp /build/config/nginx.conf /etc/nginx/nginx.conf
 mkdir -p /etc/nginx/main.d
 cp /build/config/nginx_main_d_default.conf /etc/nginx/main.d/default.conf
