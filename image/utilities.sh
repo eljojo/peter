59efc34f43138734129960bb87a41df324726641
José Tomás Albornoz <jojo@eljojo.net>
2014-06-20 16:24:38 +0200

    Disabling cron by default

--- image/utilities.sh
+++ image/utilities.sh
@@ -7,3 +7,6 @@ set -x
 $minimal_apt_get_install build-essential
 ## Bundler has to be able to pull dependencies from git.
 $minimal_apt_get_install git
+
+## Disable Cron by default
+touch /etc/service/cron/down
