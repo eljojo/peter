cc7d68f0382174086d4e45b2135109d7182c1b5b
José Tomás Albornoz <jojo@eljojo.net>
2014-06-20 19:35:53 +0200

    Fixing stuff with ruby installation

--- image/ruby.sh
+++ image/ruby.sh
@@ -9,7 +9,10 @@ tar -xzvf chruby-0.3.8.tar.gz
 cd chruby-0.3.8/
 make install
 
-echo "if [ -n \"\$BASH_VERSION\" ] || [ -n \"\$ZSH_VERSION\" ]; then\n  source /usr/local/share/chruby/chruby.sh\n  chruby use ruby\nfi">/etc/profile.d/chruby.sh
+echo "if [ -n \"\$BASH_VERSION\" ] || [ -n \"\$ZSH_VERSION\" ]; then">/etc/profile.d/chruby.sh
+echo "  source /usr/local/share/chruby/chruby.sh">>/etc/profile.d/chruby.sh
+echo "  chruby ruby">>/etc/profile.d/chruby.sh
+echo "fi">>/etc/profile.d/chruby.sh
 
 cd /tmp
 wget -O ruby-install-0.4.3.tar.gz https://github.com/postmodern/ruby-install/archive/v0.4.3.tar.gz
@@ -17,13 +20,12 @@ tar -xzvf ruby-install-0.4.3.tar.gz
 cd ruby-install-0.4.3/
 make install
 
-# echo "gem: --no-ri --no-rdoc" > /etc/gemrc
-echo "gem: --no-ri --no-rdoc" > /home/app/.gemrc
-echo "compat.version=2.0" > /home/app/.jrubyrc
+setuser app echo "gem: --no-ri --no-rdoc" > /home/app/.gemrc
+setuser app echo "compat.version=2.0" > /home/app/.jrubyrc
 
 ruby-install ruby -- --disable-install-doc
-chruby-exec ruby -- gem install bundler rake
+setuser app /usr/local/bin/chruby-exec ruby -- gem install bundler rake
 
 ruby-install jruby
-chruby-exec jruby -- gem install bundler rake
+setuser app chruby-exec jruby -- gem install bundler rake
 
